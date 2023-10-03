36bd03e227ebb223a67b98c35122a2a38c44fa5b
Teyanna Earle <teyonna.earle92@gmail.com>
2023-10-03 00:19:46 -0400

    add background img to Landing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.scss"
 
 function App() {
   return (
-    <div className="App">
+    <div id="App">
       <Routes>
         <Route path ="/" element={<Landing />} />
       </Routes>
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react"
 import "./Landing.scss"
 
 function Landing() {
   return (
-    <h1>Landing</h1>
+    <div className="Landing">
+    </div>
   )
 }
 
