2890f388c52b06ed0278a2cc3c99d059360af98b
Teyanna Earle <teyonna.earle92@gmail.com>
2023-10-05 21:04:38 -0400

    Landing scss

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,6 +4,14 @@ import "./Landing.scss"
 function Landing() {
   return (
     <div className="Landing">
+      <div className="Page__container">
+        <h1>TaskFlow</h1>
+        <div className="Landing__container">
+          <h2> Where Your Tasks Find Their Flow </h2>
+          <button>About</button>
+          <button>Google</button>
+        </div>   
+      </div>
     </div>
   )
 }
