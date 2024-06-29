5cb65badaac553b01033fb8740e5708e364e09ba
Teyanna Earle <teyonna.earle92@gmail.com>
2024-06-29 16:50:41 -0400

    add banner

--- src/util/PrivateRoute.js
+++ src/util/PrivateRoute.js
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
 
 const PrivateRoute = ({ children, ...rest }) => {
   const user = JSON.parse(window.localStorage.getItem("user"));
   return user ? (
     <>
-      <Outlet /> <Navbar />
+      <Banner /> <Outlet /> <Navbar />
     </>
   ) : (
     <Navigate to="/" />
